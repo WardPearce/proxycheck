3419fa290135876d7d65b6b09536894af38397c2
removedporn <86824510+removedporn@users.noreply.github.com>
2022-05-12 22:12:12 +0800

    Delete run_tests.py

--- run_tests.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-from proxycheck.tests.awaiting import *  # noqa: F403, F401
-from proxycheck.tests.blocking import *  # noqa: F403, F401
-
-
-if __name__ == "__main__":
-    unittest.main()
